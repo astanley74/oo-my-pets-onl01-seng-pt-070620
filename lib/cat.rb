33c08cb6673eecc8ed7fc7ee376f309248f2932b
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-29 17:25:28 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -14,4 +14,29 @@ class Cat
   def self.all
     @@all
   end
+  
+  def cats
+    
+  end
+  
+  def dogs
+  end
+  
+  def buy_cat
+  end
+  
+  def buy_dog
+  end
+  
+  def walk_dogs
+  end
+  
+  def feed_cats
+  end
+  
+  def sell_pets
+  end
+  
+  def list_pets
+  end
 end
